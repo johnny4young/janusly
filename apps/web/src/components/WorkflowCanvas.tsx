7cda060410727e718b1decbb2a3096cee8d5ddb9
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2026-04-25 14:30:32 -0500

    refactor(web): add WorkflowCanvas component

---
+++ apps/web/src/components/WorkflowCanvas.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactFlow, { Background, Controls } from '@xyflow/react'
+
+export function WorkflowCanvas({ nodes, edges, onNodesChange, onEdgesChange, onConnect, onNodeClick, onEdgeClick }: any) {
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onNodeClick={onNodeClick}
+      onEdgeClick={onEdgeClick}
+    >
+      <Background />
+      <Controls />
+    </ReactFlow>
+  )
+}
