6094220cc82d4687223dc778e06dd67f7b1700a6
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2026-04-25 09:42:38 -0500

    feat(engine): connect worker with scheduler (full DAG execution)

--- packages/engine/src/worker.ts
+++ packages/engine/src/worker.ts
@@ -2,11 +2,12 @@ import { Worker } from "bullmq";
 import { connection } from "./queue";
 import { executeNode } from "./execute-node";
 import { markNodeRunning, markNodeSucceeded, markNodeFailed, appendEvent } from "./persistence";
+import { enqueueNextNodes } from "./scheduler";
 
 export const worker = new Worker(
   "workflow-nodes",
   async (job) => {
-    const { runId, node } = job.data;
+    const { runId, node, workflow } = job.data;
 
     await markNodeRunning(runId, node.id);
 
@@ -15,6 +16,8 @@ export const worker = new Worker(
 
       await markNodeSucceeded(runId, node.id);
       await appendEvent(runId, node.id, "node.succeeded", {});
+
+      await enqueueNextNodes({ runId, workflow });
 
     } catch (err: any) {
       await markNodeFailed(runId, node.id, { message: err.message });
