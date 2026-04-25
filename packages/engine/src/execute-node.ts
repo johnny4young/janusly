61e121b8b675291656236ac5d7a1f1849c6a2f15
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2026-04-25 09:30:07 -0500

    feat(engine): add executor

---
+++ packages/engine/src/execute-node.ts
@@ -0,0 +1,17 @@
+import { nodeRegistry } from "./node-registry";
+
+export async function executeNode(input: any) {
+  const { node } = input;
+
+  const executor = nodeRegistry[node.type];
+
+  if (!executor) {
+    throw new Error(`No executor for node type: ${node.type}`);
+  }
+
+  await executor({
+    runId: input.runId,
+    nodeId: node.id,
+    config: node.config,
+  });
+}
